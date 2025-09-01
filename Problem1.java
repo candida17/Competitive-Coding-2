8df9f108c51462f5afc480d69ab69630f12e4141
candida17 <53035166+candida17@users.noreply.github.com>
2025-09-01 11:00:25 +0530

    Update Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,25 @@
+//Time Complexity - O(n) - we iterate over all n elements 
+//Space complexity - O(n)
+//Did it run in leetcode - Yes
+
+//Approach
+/*
+Use HashMap to store each element and its corresponding index.
+Calculate the complement while iterating over the elements with the target given
+Return the indices if the complement matches the element present in map
+*/
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            int diff = target - nums[i];
+            if(map.containsKey(diff)) {
+                return new int[] {map.get(diff), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[] {};
+    }
+}
